01f3671998c812a9383d2b85d260b03993c1f676
Kamil Adamski <bizkamiladamski@gmail.com>
2022-04-05 09:15:48 +0200

    Starting vommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AccordionModule,BrowserAnimationsModule,ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
