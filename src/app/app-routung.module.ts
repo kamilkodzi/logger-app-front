1fd5b8d57b84c83eb1d70f88a0146448e99ddaca
Kamil Adamski <bizkamiladamski@gmail.com>
2022-04-05 13:43:34 +0200

    first version with nav bar

---
+++ src/app/app-routung.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ErrorsTableComponent } from "./errors-table/errors-table.component";
+import { HomeViewComponent } from "./home-view/home-view.component";
+import { LogsTableComponent } from "./logs-table/logs-table.component";
+import { SettingViewComponent } from "./setting-view/setting-view.component";
+
+const routes: Routes = [
+  { path: "error-table", component: ErrorsTableComponent },
+  { path: "home", component: HomeViewComponent },
+  { path: "logs-table", component: LogsTableComponent },
+  { path: "settings", component: SettingViewComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import {AccordionModule} from 'primeng/accordion'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { AccordionModule } from "primeng/accordion";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonModule } from "primeng/button";
+import { ErrorsTableComponent } from "./errors-table/errors-table.component";
+import { MenuTabComponent } from "./menu-tab/menu-tab.component";
+import { TabMenuModule } from "primeng/tabmenu";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routung.module";
+import { HomeViewComponent } from './home-view/home-view.component';
+import { LogsTableComponent } from './logs-table/logs-table.component';
+import { SettingViewComponent } from './setting-view/setting-view.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ErrorsTableComponent, MenuTabComponent, HomeViewComponent, LogsTableComponent, SettingViewComponent],
   imports: [
-    BrowserModule,AccordionModule,BrowserAnimationsModule,ButtonModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TabMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
